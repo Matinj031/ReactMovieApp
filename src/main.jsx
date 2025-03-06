bcf05c79bd19b8d428c0535c3d97d0b7af7b4f30
Matin Jahi <Matin13822332@gmail.com>
2025-03-06 17:16:43 +0330

    Simplify routing structure in main.jsx to avoid duplicate Router components

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import Movie from './Movie.jsx'
-
 
 const root = document.getElementById('root')
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="movie/:movieId" element={<Movie />} />
-    </Routes>
+    <App />
   </BrowserRouter>
-) 
+)
